89aebed46d8223ed6ac06a8e3f7a96cc9302018e
Victoran0 <victoranosunkvic@gmail.com>
2026-01-31 18:51:20 +0000

    Replace mockNode with personaNode and add additional keyword arguments to improve agent context awareness

--- src/server/ai/agent.ts
+++ src/server/ai/agent.ts
@@ -1,30 +1,31 @@
 import { StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph";
 import { llm } from "./llm";
-import { mock } from "./prompts";
+import { personaPrompt, personaPrompts } from "./prompts";
 
 const State = new StateSchema({
   messages: MessagesValue,
 });
 
-// if the content does not seem to be a lecture note or study material, return an error
-
-const mockNode: GraphNode<typeof State> = async (state) => {
+const personaNode: GraphNode<typeof State> = async (state) => {
     console.log('The current state:\n', state.messages)
     const document = state.messages[state.messages.length - 1]?.content
+    const persona: keyof typeof personaPrompts = state.messages[state.messages.length - 1]?.additional_kwargs?.persona as keyof typeof personaPrompts
     console.log('The doc: \n', document)
-    const input = await mock.format({document: document})
-    const aiMsg = await llm.invoke(input)
+    
+    const aiMsg = await personaPrompt(persona).pipe(llm).invoke({ content: document });
     console.log('The llm response: \n', aiMsg?.toFormattedString().substring(0, 500))
     return { messages: [aiMsg] };
 };
 
 const graph = new StateGraph(State)
-  .addNode("mock_node", mockNode)
-  .addEdge(START, "mock_node")
-  .addEdge("mock_node", END)
+  .addNode("persona_node", personaNode)
+  .addEdge(START, "persona_node")
+  .addEdge("persona_node", END)
   .compile();
 
-export const invoke_agent =  async (document: string) => {
-    const response = await graph.invoke({ messages: [{ role: "user", content: document }] });
+export const invoke_agent =  async (document: string, persona: string) => {
+    const response = await graph.invoke({ messages: [{ role: "user", content: document,
+      additional_kwargs: { persona: persona }, }] });
     console.log('The AI response:\n', response)
+    return response.messages[response.messages.length -1]?.content;
 }
