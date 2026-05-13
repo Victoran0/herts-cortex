c9dc2a4879d994dee895b056544213474515a6f8
Victoran0 <victoranosunkvic@gmail.com>
2026-05-13 19:08:24 +0100

    Create multiNode agent to route to different nodes based on the input persona

--- src/server/ai/agent.ts
+++ src/server/ai/agent.ts
@@ -1,31 +1,97 @@
-import { StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph";
+import { MemorySaver, StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph";
 import { llm } from "./llm";
 import { personaPrompt, personaPrompts } from "./prompts";
+import { mcqSchema } from "./validator";
+import { AIMessage } from "node_modules/@langchain/core/dist/messages/ai";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const State = new StateSchema({
   messages: MessagesValue,
 });
 
-const personaNode: GraphNode<typeof State> = async (state) => {
-    console.log('The current state:\n', state.messages)
-    const document = state.messages[state.messages.length - 1]?.content
-    const persona: keyof typeof personaPrompts = state.messages[state.messages.length - 1]?.additional_kwargs?.persona as keyof typeof personaPrompts
-    console.log('The doc: \n', document)
+const routePersona = async (state: typeof State.State) => {
+    // console.log('The current state:\n', state.messages)
+    // Get the persona from the most recent message (the initial user message)
+    const lastMessage = state.messages[state.messages.length - 1];
+    const persona: keyof typeof personaPrompts = lastMessage?.additional_kwargs?.persona as keyof typeof personaPrompts
     
-    const aiMsg = await personaPrompt(persona).pipe(llm).invoke({ content: document });
-    console.log('The llm response: \n', aiMsg?.toFormattedString().substring(0, 500))
-    return { messages: [aiMsg] };
+    if (persona === "mcq") {
+      return "mcqNode"
+    }
+
+    return 'multiPersonaNode'
 };
 
-const graph = new StateGraph(State)
-  .addNode("persona_node", personaNode)
-  .addEdge(START, "persona_node")
-  .addEdge("persona_node", END)
-  .compile();
+const mcqNode: GraphNode<typeof State> = async (state) => {
+    const lastMessage = state.messages[state.messages.length - 1];
+     const message = lastMessage?.content as string;
+     const document = lastMessage?.additional_kwargs?.document as string;
+     if (!document || !message) {
+        throw new Error("Missing document or message in the state.");
+    }
 
-export const invoke_agent =  async (document: string, persona: string) => {
-    const response = await graph.invoke({ messages: [{ role: "user", content: document,
-      additional_kwargs: { persona: persona }, }] });
-    console.log('The AI response:\n', response)
-    return response.messages[response.messages.length -1]?.content;
+    const llmWithStructuredOutput = llm.withStructuredOutput(mcqSchema, {
+        name: "generate_mcq"
+    });
+
+    const structuredResponse = await personaPrompt("mcq").pipe(llmWithStructuredOutput).invoke({ content: document, query: message });
+    
+    return { 
+        messages:[new AIMessage({ content: JSON.stringify(structuredResponse) })] 
+    };
+};
+
+const multiPersonaNode: GraphNode<typeof State> = async (state) => {
+    const message = state.messages[state.messages.length - 1]?.content as string;
+    const lastMessage = state.messages[state.messages.length - 1];
+    const document = lastMessage?.additional_kwargs?.document as string;
+    const persona = lastMessage?.additional_kwargs?.persona as keyof typeof personaPrompts;
+
+    if (!document || !message) {
+        throw new Error("Missing document or message in the state.");
+    }
+
+    const aiMsg = await personaPrompt(persona).pipe(llm).invoke({ content: document, query: message });
+    return { messages: [aiMsg] };
 }
+
+const checkpointer = new MemorySaver();
+
+export const graph = new StateGraph(State)
+  .addNode("mcqNode", mcqNode)
+  .addNode("multiPersonaNode", multiPersonaNode)
+  .addConditionalEdges(START, routePersona, {
+      mcqNode: "mcqNode",
+      multiPersonaNode: "multiPersonaNode"
+  })
+  .addEdge("mcqNode", END)
+  .addEdge("multiPersonaNode", END)
+  .compile({ checkpointer });
+
+// 6. Invoke the Agent
+export const invoke_agent = async (document: string, persona: string, message: string) => {
+    const thread_id = uuidv4();
+    const config = { configurable: {thread_id: thread_id } };
+  //  Only pass the document when the state is empty, i.e new convo after adding memory
+    const response = await graph.invoke({ 
+        messages:[{ 
+            role: "user", 
+            content: message,
+            additional_kwargs: { persona: persona, document: document }, 
+        }] 
+    }, config
+    );
+    const graphState = await graph.getState(config)
+    console.log('The current state of the graph:\n', graphState);
+    console.log('The AI response:\n', response);
+    
+    const finalContent = response.messages[response.messages.length - 1]?.content;
+
+    // If you routed to the mcqNode, parse the structured string back into a JS object
+    if (persona === "mcq" && typeof finalContent === "string") {
+        return JSON.parse(finalContent); 
+    }
+
+    return finalContent;
+};
